89ab2f7855acf791934ec4c413bb5bb8de8f36e9
WissamDouskary <douskary.wissam@gmail.com>
2025-12-24 15:50:25 +0100

    feat: create api and error response to handle my json responses

---
+++ src/main/java/com/brickbase/platform/dto/ApiResponse.java
@@ -0,0 +1,17 @@
+package com.brickbase.platform.dto;
+
+import lombok.*;
+import org.springframework.http.HttpStatus;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+
+    private int status;
+    private String title;
+    private String message;
+    private HttpStatus httpStatus;
+    private T data;
+}
