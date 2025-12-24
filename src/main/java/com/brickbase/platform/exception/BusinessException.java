3fbd6727800c68c14e8ca319c67f12f83e0ab072
WissamDouskary <douskary.wissam@gmail.com>
2025-12-24 15:51:06 +0100

    feat: create a GlobalExceptionHandler to handle any exception in my project and return a structured response

---
+++ src/main/java/com/brickbase/platform/exception/BusinessException.java
@@ -0,0 +1,19 @@
+package com.brickbase.platform.exception;
+
+import lombok.Getter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+public class BusinessException extends RuntimeException {
+    private final HttpStatus httpStatus;
+
+    public BusinessException(String message, HttpStatus httpStatus) {
+        super(message);
+        this.httpStatus = httpStatus;
+    }
+
+    public BusinessException(String message) {
+        super(message);
+        this.httpStatus = HttpStatus.BAD_REQUEST;
+    }
+}
