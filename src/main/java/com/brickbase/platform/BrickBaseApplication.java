bd9950a1cd89569e8176b8c77e4d46225d642370
WissamDouskary <douskary.wissam@gmail.com>
2025-12-24 09:10:09 +0100

    init: initialising spring boot project

---
+++ src/main/java/com/brickbase/platform/BrickBaseApplication.java
@@ -0,0 +1,11 @@
+package com.brickbase.platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrickBaseApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BrickBaseApplication.class, args);
+    }
+}
---
+++ src/test/java/com/brickbase/platform/BrickBaseFileStructureApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brickbase.platform;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BrickBaseFileStructureApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
