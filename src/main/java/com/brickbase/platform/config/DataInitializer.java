0e6f49acf227cf51b754fbd9997d28a7c1342bd0
WissamDouskary <douskary.wissam@gmail.com>
2025-12-26 22:16:03 +0100

    feat: create initializer method in init admin if not exists for test

--- src/main/java/com/brickbase/platform/config/DataInitializer.java
+++ src/main/java/com/brickbase/platform/config/DataInitializer.java
@@ -4,17 +4,24 @@ import com.brickbase.platform.exception.ResourceNotFoundException;
 import com.brickbase.platform.model.Permission;
 import com.brickbase.platform.model.Role;
 import com.brickbase.platform.enums.RoleName;
+import com.brickbase.platform.model.User;
 import com.brickbase.platform.repository.PermissionRepository;
 import com.brickbase.platform.repository.RoleRepository;
+import com.brickbase.platform.repository.UserRepository;
+import lombok.AllArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.HashSet;
 import java.util.Set;
 
 @Configuration
+@AllArgsConstructor
 public class DataInitializer {
+    private final PasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
 
     @Bean
     CommandLineRunner initData(PermissionRepository permissionRepository, RoleRepository roleRepository) {
@@ -45,7 +52,7 @@ public class DataInitializer {
             createRoleIfNotExists(roleRepository, RoleName.ROLE_ADMIN, allPermissions);
 
             Set<Permission> workerPermissions = new HashSet<>();
-            for (String perm : new String[] {
+            for (String perm : new String[]{
                     "PRODUCT_READ", "PRODUCT_WRITE", "PRODUCT_DELETE",
                     "ORDER_READ", "ORDER_WRITE",
                     "WORKER_READ", "WORKER_WRITE",
@@ -57,7 +64,7 @@ public class DataInitializer {
             createRoleIfNotExists(roleRepository, RoleName.ROLE_WORKER, workerPermissions);
 
             Set<Permission> clientPermissions = new HashSet<>();
-            for (String perm : new String[] {
+            for (String perm : new String[]{
                     "PRODUCT_READ",
                     "ORDER_READ", "ORDER_WRITE",
                     "REVIEW_READ", "REVIEW_WRITE",
@@ -66,6 +73,11 @@ public class DataInitializer {
                 clientPermissions.add(permissionRepository.findByName(perm));
             }
             createRoleIfNotExists(roleRepository, RoleName.ROLE_CLIENT, clientPermissions);
+            if (roleRepository.findByName(RoleName.ROLE_ADMIN).isPresent()) {
+                if (roleRepository.findByName(RoleName.ROLE_ADMIN).get().getName().equals(RoleName.ROLE_ADMIN)) {
+                    createAdminIfNotExists("admin@gmail.com", roleRepository);
+                }
+            }
         };
     }
 
@@ -77,6 +89,20 @@ public class DataInitializer {
             role.setName(roleName);
             role.setPermissions(permissions);
             roleRepository.save(role);
+        }
+    }
+
+    private void createAdminIfNotExists(String adminEmail, RoleRepository roleRepository) {
+        User user = userRepository.findByEmail(adminEmail)
+                .orElse(null);
+        if (user == null) {
+            user = new User();
+            user.setFirstName("Admin");
+            user.setLastName("admin");
+            user.setEmail(adminEmail);
+            user.setPassword(passwordEncoder.encode("123456789"));
+            user.setRole(roleRepository.findByName(RoleName.ROLE_ADMIN).orElseThrow(() -> new ResourceNotFoundException("Role not found!")));
+            userRepository.save(user);
         }
     }
 }
