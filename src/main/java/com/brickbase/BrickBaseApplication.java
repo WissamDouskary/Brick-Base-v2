899de70e9f3811beb98d5c18c244cd027ca5cf78
WissamDouskary <douskary.wissam@gmail.com>
2025-12-26 09:37:40 +0100

    feat: add EnableJpaAuditing annotation to application

--- src/main/java/com/brickbase/BrickBaseApplication.java
+++ src/main/java/com/brickbase/BrickBaseApplication.java
@@ -1,9 +1,11 @@
-package com.brickbase.platform;
+package com.brickbase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class BrickBaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(BrickBaseApplication.class, args);
